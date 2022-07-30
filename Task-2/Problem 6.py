24b22f20977affdbe3b643cefdc78bfe94fb79d5
VDhakane <99000757+VDhakane@users.noreply.github.com>
2022-07-30 21:37:10 +0530

    Delete Task-2 directory

--- Task-2/Problem 6.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    n = int(raw_input())
-    integer_list = map(int, raw_input().split())
-    lists = [] 
-    for i in integer_list:
-        lists.append(i)
-        
-    print(hash(tuple(lists)))
