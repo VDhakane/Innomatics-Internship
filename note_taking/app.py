7d96b6d58ce7002f628e938aa7aa2adf06b9ab31
VDhakane <99000757+VDhakane@users.noreply.github.com>
2022-08-15 07:26:03 -0700

    Add files via upload

---
+++ note_taking/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request , url_for, redirect
+
+app = Flask(__name__)
+
+notes = []
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        note= request.form["note"]
+        notes.append(note)
+        return redirect(url_for('index'))
+
+    return render_template("home.html", notes=notes)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
